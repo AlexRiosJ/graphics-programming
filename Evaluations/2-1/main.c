f43cda65f60b68cddbc472cbff99eeb599a4fc96
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-03-19 14:40:03 -0600

    Adding Contest excercises 1 and 2

---
+++ Evaluations/2-1/main.c
@@ -0,0 +1,238 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "utils.h"
+#include "transforms.h"
+#include <stdio.h>
+#include <math.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#define TOTAL_POINTS 628
+
+static GLuint programId, va[1], vertexPosLoc, vertexColLoc, modelMatrixLoc, viewMatrixLoc, projMatrixLoc;
+static Mat4 projMatrix;
+static GLboolean usePerspective = GL_TRUE;
+static float angleY = 0, angleZ = 0;
+typedef enum
+{
+	NONE,
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+} Motion;
+Motion motion = NONE;
+static float cameraX = 0;
+static float cameraZ = 0;
+static float cameraAngle = 0;
+static float cameraSpeed = 0.2;
+static float rotationSpeed = 2;
+
+static void initShaders()
+{
+	GLuint vShader = compileShader("shaders/projection.vsh", GL_VERTEX_SHADER);
+	if (!shaderCompiled(vShader))
+		return;
+	GLuint fShader = compileShader("shaders/color.fsh", GL_FRAGMENT_SHADER);
+	if (!shaderCompiled(fShader))
+		return;
+
+	programId = glCreateProgram();
+	glAttachShader(programId, vShader);
+	glAttachShader(programId, fShader);
+	glLinkProgram(programId);
+	vertexPosLoc = glGetAttribLocation(programId, "vertexPosition");
+	vertexColLoc = glGetAttribLocation(programId, "vertexColor");
+	modelMatrixLoc = glGetUniformLocation(programId, "modelMatrix");
+	viewMatrixLoc = glGetUniformLocation(programId, "viewMatrix");
+	projMatrixLoc = glGetUniformLocation(programId, "projMatrix");
+
+	glUseProgram(programId);
+	glEnable(GL_DEPTH_TEST);
+	//	glEnable(GL_CULL_FACE);
+	//	glFrontFace(GL_CW);
+}
+
+// Crea un cuarto de (w x h x d) centrado en el origen
+static void createPlot()
+{
+	static float points[TOTAL_POINTS * 6];
+	static float colors[TOTAL_POINTS * 6];
+	float x = -4 * M_PI;
+	for (int i = 0; i < TOTAL_POINTS * 6; i += 6, x += 8 * M_PI / TOTAL_POINTS * 2)
+	{
+		points[i] = x;
+		points[i + 1] = exp(cos(2 * x));
+		points[i + 2] = -0.4;
+
+		points[i + 3] = x;
+		points[i + 4] = exp(cos(2 * x));
+		points[i + 5] = 0.4;
+
+		colors[i] = 1;
+		colors[i + 1] = (1 + cos(x)) / 2.0;
+		colors[i + 2] = 0;
+
+		colors[i + 3] = 0;
+		colors[i + 4] = (1 + cos(x)) / 2.0;
+		colors[i + 5] = 1;
+		// printf("%.2f, %.2f, %.2f\n", points[i], points[i + 1], points[i + 2]);
+		// printf("%.2f, %.2f, %.2f\n", points[i + 3], points[i + 4], points[i + 5]);
+	}
+
+	glGenVertexArrays(1, va);
+	glBindVertexArray(va[0]);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 1);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(vertexPosLoc);
+	glVertexAttribPointer(vertexPosLoc, 3, GL_FLOAT, 0, 0, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(vertexColLoc);
+	glVertexAttribPointer(vertexColLoc, 3, GL_FLOAT, 0, 0, 0);
+}
+
+static void moveForward()
+{
+	float radians = M_PI * cameraAngle / 180;
+	cameraX -= cameraSpeed * sin(radians);
+	cameraZ -= cameraSpeed * cos(radians);
+}
+
+static void moveBackward()
+{
+	float radians = M_PI * cameraAngle / 180;
+	cameraX += cameraSpeed * sin(radians);
+	cameraZ += cameraSpeed * cos(radians);
+}
+
+static void rotateLeft()
+{
+	cameraAngle += rotationSpeed;
+}
+
+static void rotateRight()
+{
+	cameraAngle -= rotationSpeed;
+}
+
+static void display()
+{
+	Mat4 modelMat;
+	Mat4 viewMat;
+	mIdentity(&modelMat);
+	mIdentity(&viewMat);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(programId);
+
+	switch (motion)
+	{
+	case FORWARD:
+		moveForward();
+		break;
+	case BACKWARD:
+		moveBackward();
+		break;
+	case LEFT:
+		rotateLeft();
+		break;
+	case RIGHT:
+		rotateRight();
+	}
+	rotateY(&viewMat, -cameraAngle);
+	translate(&viewMat, -cameraX, 0, -cameraZ);
+
+	glBindVertexArray(va[0]);
+	glUniformMatrix4fv(viewMatrixLoc, 1, GL_TRUE, viewMat.values);
+	glUniformMatrix4fv(modelMatrixLoc, 1, GL_TRUE, modelMat.values);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, TOTAL_POINTS);
+	glutSwapBuffers();
+}
+
+static void timerFunc(int id)
+{
+	glutTimerFunc(20, timerFunc, id);
+	glutPostRedisplay();
+}
+
+static void reshapeFunc(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	float aspect = (float)w / h;
+	if (usePerspective)
+	{
+		setPerspective(&projMatrix, 60, aspect, -1, -200);
+	}
+	else
+	{
+		if (aspect >= 1.0)
+			setOrtho(&projMatrix, -6 * aspect, 6 * aspect, -6, 6, -6, 6);
+		else
+			setOrtho(&projMatrix, -6, 6, -6 / aspect, 6 / aspect, -6, 6);
+	}
+	glUniformMatrix4fv(projMatrixLoc, 1, GL_TRUE, projMatrix.values);
+}
+
+static void exitFunc(unsigned char key, int x, int y)
+{
+	if (key == 27)
+	{
+		glDeleteVertexArrays(1, va);
+		exit(0);
+	}
+	if (key == 13)
+	{
+		usePerspective = !usePerspective;
+		int w = glutGet(GLUT_WINDOW_WIDTH);
+		int h = glutGet(GLUT_WINDOW_HEIGHT);
+		reshapeFunc(w, h);
+	}
+}
+
+static void specialKeyPressed(int code, int x, int y)
+{
+	switch (code)
+	{
+	case 101:
+		motion = FORWARD;
+		break;
+	case 103:
+		motion = BACKWARD;
+		break;
+	case 100:
+		motion = LEFT;
+		break;
+	case 102:
+		motion = RIGHT;
+	}
+}
+
+static void specialKeyReleased(int code, int x, int y)
+{
+	motion = NONE;
+}
+
+int main(int argc, char **argv)
+{
+	setbuf(stdout, NULL);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(100, 100);
+	glutTimerFunc(50, timerFunc, 1);
+
+	glutCreateWindow("Practice 1");
+	glutDisplayFunc(display);
+	glutKeyboardFunc(exitFunc);
+	glutSpecialFunc(specialKeyPressed);
+	glutSpecialUpFunc(specialKeyReleased);
+	glutReshapeFunc(reshapeFunc);
+	glewInit();
+	initShaders();
+	createPlot();
+	glClearColor(0.05, 0.05, 0.10, 1.0);
+	glutMainLoop();
+	return 0;
+}
